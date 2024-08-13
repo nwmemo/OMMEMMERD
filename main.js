f8708d7472a65452d740143b35ddd24043d4f494
Memo <165202034+nwmemo@users.noreply.github.com>
2024-08-13 20:20:57 +0200

    Version 1.0
    The game is much more random now!

--- main.js
+++ main.js
@@ -36,8 +36,14 @@ if(cat == 0){
 }
 
 const numbPlayers = localStorage.getItem('sharedValue');
-console.log(numbPlayers);
+console.log("number of players: " + numbPlayers);
 let word = "";
+
+const numPlayer = numbPlayers;
+let playersArray = [];
+
+
+
 
 function generatore() {
     if(cat == 0){ 
@@ -64,50 +70,54 @@ function generatore() {
 };
 let extractedWord = word;
 
+for(let k = 1; k <= numPlayer; k++){
+    playersArray.push("Player " + k);
+};
+k = 0;
+
+let impostor = Math.floor(Math.random()*numPlayer + 1);
+console.log("the imposotr is number: " + impostor);
+if(impostor == numPlayer + 1){
+    impostor = numPlayer;
+};
+
 let btn = document.getElementById('btn');
 let counter = 1;
 let display = document.getElementById("display");
-let twoShown = false;
-let x = 0;
+let x = 1;
 
 
 
 btn.addEventListener('click', function(){
-    if(x <= numbPlayers){
-         if(counter == 1){
+    if(x <= numPlayer ){
+        if(counter == 1){
             counter++;
             document.getElementById('title').innerHTML = "La vostra parola e': ";
             btn.innerText = "Premi per oscurare";
-            x++;
-            if(twoShown == true){
-                display.innerText = word;
-                
-            }else {
-                let coin =  Math.random();
-                if (coin < 0.5){
-                    display.innerText = word;
-
+                if(playersArray[x] == playersArray[impostor]){
+                    display.innerHTML = "Omm e' Merd"
                 }else{
-                    display.innerText = "Omm e' mmerd";
-                    btn.innerText = "Premi per oscurare";
-                    twoShown = true;
-                    x++;
+                    display.innerHTML = word;
                 };
-                
-            }
+x++;
         }else{
             document.getElementById('title').innerHTML = "Parola Nascosta";
             display.innerHTML = "Passa il telefono alla prossima persona";
             display.style.fontSize = "medium";
-            btn.innerText = "Premi per mostrare"
-            counter = 1; 
-        }
-}else{
-    document.getElementById('title').innerHTML = "Buona Fortuna!";
-    display.innerHTML = "Iniziate a farvi domande!";
-}
+            btn.innerText = "Premi per mostrare";
+            counter = 1;
+        };
+    }else{
+        document.getElementById('title').innerHTML = "Buona Fortuna!";
+        display.innerHTML = "Iniziate a farvi domande!";
+        };
+
+});
+
+
+
     
-})
+
 
 
 homeButton.addEventListener('click', function() {
